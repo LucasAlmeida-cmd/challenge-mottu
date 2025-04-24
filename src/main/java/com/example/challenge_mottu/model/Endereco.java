e4af351c644c08dc179a0b81b2738546980b026f
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-04-24 10:15:00 -0300

    feat: criação da clases(Endereco/EnderecoDTO)

---
+++ src/main/java/com/example/challenge_mottu/model/Endereco.java
@@ -0,0 +1,87 @@
+package com.example.challenge_mottu.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Endereco {
+
+    @Column(name = "cep")
+    private String cep;
+
+    @Column(name = "logradouro")
+    private String logradouro;
+
+    @Column(name = "complemento")
+    private String complemento;
+
+    @Column(name = "bairro")
+    private String bairro;
+
+    @Column(name = "localidade")
+    private String localidade;
+
+    @Column(name = "uf")
+    private String uf;
+
+    public Endereco(String cep, String logradouro, String complemento, String bairro, String localidade, String uf) {
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.localidade = localidade;
+        this.uf = uf;
+    }
+
+    public Endereco() {
+
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+}
