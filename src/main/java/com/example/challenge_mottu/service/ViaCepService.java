5c4c2d9c1c58c2cbde377497287ae846c2e79373
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-04-24 10:15:39 -0300

    feat: criação da service(ViaCepService)

---
+++ src/main/java/com/example/challenge_mottu/service/ViaCepService.java
@@ -0,0 +1,38 @@
+package com.example.challenge_mottu.service;
+
+import com.example.challenge_mottu.dto.EnderecoDTO;
+import com.example.challenge_mottu.model.Endereco;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ViaCepService {
+
+    private final RestTemplate restTemplate;
+
+    public ViaCepService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+   public Endereco buscarEnderecoPorCEP(String cep){
+       String urlVia = "viacep.com.br/ws/"+ cep +"/json/";
+
+       EnderecoDTO response = restTemplate.getForObject(urlVia, EnderecoDTO.class);
+
+       if (response == null) {
+           throw new RuntimeException("CEP não encontrado");
+       }
+
+       return new Endereco(
+               response.getCep(),
+               response.getLogradouro(),
+               response.getComplemento(),
+               response.getBairro(),
+               response.getLocalidade(),
+               response.getUf()
+       );
+   }
+
+
+}
