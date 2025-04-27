4f8f610630a2817240372449449316e249b7b3b9
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-04-27 15:01:36 -0300

    feat: novas funcionalidades para admin e para motoqueiro

---
+++ src/main/java/com/example/challenge_mottu/repository/MotoRepository.java
@@ -0,0 +1,7 @@
+package com.example.challenge_mottu.repository;
+
+import com.example.challenge_mottu.model.Moto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MotoRepository extends JpaRepository<Moto, Long> {
+}
---
+++ src/main/java/com/example/challenge_mottu/repository/MotoqueiroRepository.java
@@ -0,0 +1,7 @@
+package com.example.challenge_mottu.repository;
+
+import com.example.challenge_mottu.model.Motoqueiro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MotoqueiroRepository extends JpaRepository<Motoqueiro, Long> {
+}
