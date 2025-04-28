c4e716489ba1645fdaffc352b569d7f9aad3bcb8
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-04-28 11:45:53 -0300

    refactor: criação de metodo de busca

--- src/main/java/com/example/challenge_mottu/repository/MotoqueiroRepository.java
+++ src/main/java/com/example/challenge_mottu/repository/MotoqueiroRepository.java
@@ -1,7 +1,9 @@
 package com.example.challenge_mottu.repository;
 
+import com.example.challenge_mottu.model.Administrador;
 import com.example.challenge_mottu.model.Motoqueiro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MotoqueiroRepository extends JpaRepository<Motoqueiro, Long> {
+    Motoqueiro findByCpfUser(String cpf);
 }
