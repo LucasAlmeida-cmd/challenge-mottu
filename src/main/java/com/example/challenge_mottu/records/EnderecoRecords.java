4d293e6d83c8d207adac0d9521e2720131efd432
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-04-29 10:11:59 -0300

    refactor: troca de DTO para Records

---
+++ src/main/java/com/example/challenge_mottu/records/EnderecoRecords.java
@@ -0,0 +1,9 @@
+package com.example.challenge_mottu.records;
+
+public record EnderecoRecords(String cep,
+                              String logradouro,
+                              String complemento,
+                              String bairro,
+                              String localidade,
+                              String uf){
+}
