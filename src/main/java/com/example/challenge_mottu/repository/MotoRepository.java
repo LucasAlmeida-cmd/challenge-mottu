e7ab8820f59411d1641913c7b65cc85b46042b4d
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-04-29 10:15:38 -0300

    feat: criação de busca personalizada

--- src/main/java/com/example/challenge_mottu/repository/MotoRepository.java
+++ src/main/java/com/example/challenge_mottu/repository/MotoRepository.java
@@ -2,6 +2,9 @@ package com.example.challenge_mottu.repository;
 
 import com.example.challenge_mottu.model.Moto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface MotoRepository extends JpaRepository<Moto, Long> {
+    Moto findByChassi(String chassi);
 }
