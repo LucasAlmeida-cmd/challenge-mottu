9259f13975f7aec3dffe5792f620e87e32e603e1
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-05-01 17:17:31 -0300

    feat: nova entidade patio até o controller

---
+++ src/main/java/com/example/challenge_mottu/controller/PatioController.java
@@ -0,0 +1,52 @@
+package com.example.challenge_mottu.controller;
+
+import com.example.challenge_mottu.model.Patio;
+import com.example.challenge_mottu.records.PatioRecord;
+import com.example.challenge_mottu.service.PatioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patio")
+public class PatioController {
+
+
+    @Autowired
+    PatioService patioService;
+
+    @PostMapping
+    public ResponseEntity<Patio> adicionar(@RequestBody Patio patio){
+        patioService.adicionar(patio);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{ident}")
+    public ResponseEntity<Patio>buscarPorIden(@PathVariable String ident) {
+        return ResponseEntity.ok(patioService.buscarPorInd(ident));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatioRecord>> listarTodos() {
+        return ResponseEntity.ok(patioService.listarTodos());
+    }
+
+    @PutMapping("/{indentificacao}")
+    public ResponseEntity<Patio> atualizar(@PathVariable String indentificacao, @RequestBody Patio patio){
+        return ResponseEntity.ok(patioService.atualizarPatio(indentificacao, patio));
+    }
+
+    @DeleteMapping("/{indentificacao}")
+    public ResponseEntity<Patio> deletar(@PathVariable String indentificacao){
+        patioService.remover(indentificacao);
+        return ResponseEntity.ok().build();
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/example/challenge_mottu/model/Patio.java
@@ -0,0 +1,74 @@
+package com.example.challenge_mottu.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_patio")
+public class Patio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPatio;
+
+    @Column(nullable = false, unique = true)
+    private String identificacao;
+
+    private double largura;
+    private double comprimento;
+
+    @OneToMany(mappedBy = "patio", cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<Secao> secoes = new ArrayList<>();
+
+
+    public Patio(double largura, double comprimento, List<Secao> secoes,String identificacao) {
+        this.largura = largura;
+        this.comprimento = comprimento;
+        this.secoes = secoes;
+        this.identificacao = identificacao;
+    }
+
+    public Patio(){}
+
+    public String getIdentificacao() {
+        return identificacao;
+    }
+
+    public void setIdentificacao(String identificacao) {
+        this.identificacao = identificacao;
+    }
+
+    public Long getIdPatio() {
+        return idPatio;
+    }
+
+    public void setIdPatio(Long idPatio) {
+        this.idPatio = idPatio;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+
+    public double getComprimento() {
+        return comprimento;
+    }
+
+    public void setComprimento(double comprimento) {
+        this.comprimento = comprimento;
+    }
+
+    public List<Secao> getSecoes() {
+        return secoes;
+    }
+
+    public void setSecoes(List<Secao> secoes) {
+        this.secoes = secoes;
+    }
+}
